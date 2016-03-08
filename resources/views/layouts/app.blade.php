69ec193f5eb87dd88cd9a0705cad7467892f015b
EC2 Default User <ec2-user@ip-172-31-62-74.ec2.internal>
2016-03-08 16:10:35 +0000

    Added login button to main menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,6 +72,20 @@
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
                         </li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                Log in <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    <a href= "{{ url('/login/eveonline') }}">
+                                        <img src="{{ url('/images/ssologin_small.png') }}" alt="Login with Eve Online"/>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
                     @endif
                 </ul>
             </div>
